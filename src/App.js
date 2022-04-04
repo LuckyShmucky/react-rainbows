82f5fe9749d2ed75554760ae743a57fa242b70b1
LuckyShmucky <christianluciano567@gmail.com>
2022-04-04 16:10:56 -0700

    some of the bonus material executed or changed to

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 import './App.css';
+import React, { useState } from 'react';
+import ColorBlock from './components/color-block/index'
+import ColorForm from './components/color-form';
+
 
 function App() {
+  let [colors, setColors] = useState([
+    // 'violet', 'blue',
+    // 'lightblue', 'green',
+    // 'greenyellow', 'yellow',
+    // 'orange', 'red'
+///make empty string and iterate over it
+  ])
+  
+  
+  const colorMap = colors.map((color) => {
+    // console.log(color)
+    return <div key={color}>
+      <ColorBlock className='w-50' color={color}/>
+    </div>
+  })
+
+  const addColor = (newColor) =>{
+    ///to get rid of current color and replace it with the user's input
+    setColors([newColor])
+    // setColors([...colors, newColor])
+    console.log(newColor)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       {colorMap}
+       <ColorForm addColor={addColor}/>
     </div>
   );
 }
